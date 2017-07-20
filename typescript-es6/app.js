5da3e602ddeeeda4be027fe18de20e682c789b01
Alejandro Irimia <rimax_86@hotmail.com>
2017-07-20 20:06:37 +0200

    Variables let y const

--- typescript-es6/app.js
+++ typescript-es6/app.js
@@ -1,7 +1,12 @@
-function saludar(nombre) {
-    console.log("Hola " + nombre.toUpperCase());
+var mensaje = "hola";
+// let mensaje = "adios"; No se puede declarar dos veces la misma variable
+if (true) {
+    var mensaje_1 = "adios";
 }
-var wolverine = {
-    nombre: "Logan"
-};
-saludar(wolverine.nombre);
+console.log(mensaje); // Imprime hola
+var OPCIONES = "todas";
+// OPCIONES = "ninguna"; No se puede cambiar el valor de una constante
+// const OTRACONSTANTE: No se puede declarar una constante sin inicializarla
+if (true) {
+    var OPCIONES_1 = "ninguna"; // Se puede redeclarar en otro Scope
+}
--- typescript-es6/app.ts
+++ typescript-es6/app.ts
@@ -1,9 +1,17 @@
-function saludar(nombre:string) {
-  console.log("Hola " + nombre.toUpperCase());
+let mensaje = "hola";
+// let mensaje = "adios"; No se puede declarar dos veces la misma variable
+
+if(true) {
+  let mensaje = "adios";
 }
 
-var wolverine = {
-  nombre: "Logan"
-};
+console.log(mensaje); // Imprime hola
 
-saludar(wolverine.nombre);
+const OPCIONES = "todas";
+
+// OPCIONES = "ninguna"; No se puede cambiar el valor de una constante
+// const OTRACONSTANTE: No se puede declarar una constante sin inicializarla
+
+if(true) {
+  const OPCIONES = "ninguna"; // Se puede redeclarar en otro Scope
+}
