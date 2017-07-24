b34c38882df02ff346e8154e4d5b093713b0ae12
Alejandro Irimia <rimax_86@hotmail.com>
2017-07-24 21:05:40 +0200

    Decoradores de Clases

--- typescript-es6/app.js
+++ typescript-es6/app.js
@@ -1,9 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import {Xmen} from './classes/xmen.class';
-//import {Villano} from './classes/villano.class';
-var index_1 = require("./classes/index");
-var wolverine = new index_1.Xmen("Logan", "wolverine");
-var lex = new index_1.Villano("Lex Luthor", "Conquistar mundo");
-wolverine.imprimir();
-lex.imprimirPlan();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function consola(constructor) {
+    console.log(constructor);
+}
+var Villano = (function () {
+    function Villano(nombre) {
+        this.nombre = nombre;
+    }
+    Villano = __decorate([
+        consola
+    ], Villano);
+    return Villano;
+}());
--- typescript-es6/app.ts
+++ typescript-es6/app.ts
@@ -1,10 +1,9 @@
-//import {Xmen} from './classes/xmen.class';
-//import {Villano} from './classes/villano.class';
-import {Xmen, Villano} from './classes/index';
+function consola(constructor:Function) {
+  console.log(constructor);
+}
 
-
-let wolverine = new Xmen("Logan", "wolverine");
-let lex = new Villano("Lex Luthor", "Conquistar mundo");
-
-wolverine.imprimir();
-lex.imprimirPlan();
+@consola
+class Villano {
+  constructor(public nombre:string, as:string){
+  }
+}
