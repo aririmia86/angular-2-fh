d852a76e028ecda3cd8e2882087cee45c6c037af
Alejandro Irimia <rimax_86@hotmail.com>
2017-07-24 20:32:34 +0200

    Constructores de una clase en TypeScript

--- typescript-es6/app.js
+++ typescript-es6/app.js
@@ -1,8 +1,15 @@
 var Avenger = (function () {
-    function Avenger() {
-        this.nombre = "Antman";
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = "Sin nombre";
+        this.equipo = undefined;
+        this.nombreReal = undefined;
+        this.puedePelear = false;
+        this.peleasGanadas = 0;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
     }
     return Avenger;
 }());
-var antman = new Avenger();
+var antman = new Avenger("Antman", "Capitan", "Scott Lang");
 console.log(antman);
--- typescript-es6/app.ts
+++ typescript-es6/app.ts
@@ -1,12 +1,18 @@
 class Avenger {
-  nombre:string = "Antman";
-  equipo:string;
-  nombreReal:string;
+  nombre:string = "Sin nombre";
+  equipo:string = undefined;
+  nombreReal:string = undefined;
 
-  puedePelear:boolean;
-  peleasGanadas:number;
+  puedePelear:boolean = false;
+  peleasGanadas:number = 0;
+
+  constructor(nombre:string, equipo:string, nombreReal:string) {
+    this.nombre = nombre;
+    this.equipo = equipo;
+    this.nombreReal = nombreReal;
+  }
 }
 
-let antman:Avenger = new Avenger();
+let antman:Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
 
 console.log(antman);
