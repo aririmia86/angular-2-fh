6057097684b9ec3131aa0ce3fe4681a25bd16a1f
Alejandro Irimia <rimax_86@hotmail.com>
2017-08-02 20:19:22 +0200

    Buscador de Héroes

--- 2. SPA/src/app/services/heroes.service.ts
+++ 2. SPA/src/app/services/heroes.service.ts
@@ -63,6 +63,18 @@ export class HeroesService {
     return this.heroes[id];
   }
 
+  buscarHeroes(termino:string):Heroe[] {
+    let heroesArr:Heroe[] = [];
+    termino = termino.toLowerCase();
+    for(let heroe of this.heroes) {
+      let nombre = heroe.nombre.toLowerCase();
+      if(nombre.indexOf(termino) >= 0){
+        heroesArr.push(heroe);
+      }
+    }
+    return heroesArr;
+  }
+
   constructor() {
     // console.log("Servicio listo para usar");
   }
