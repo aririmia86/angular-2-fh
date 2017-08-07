13f60b837c5249d2235564fb878b52c850b8f1c5
Alejandro Irimia <rimax_86@hotmail.com>
2017-08-07 19:49:43 +0200

    Pipe: Decimal

--- 3. Pipes/src/app/app.component.ts
+++ 3. Pipes/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre:string = "Alejandro";
   array:number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  pi:number = Math.PI;
 }
