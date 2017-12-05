8fad3426bd0a78d4cf8e33377e313e2f7aa0b934
Alejandro Irimia <rimax_86@hotmail.com>
2017-12-05 12:04:43 +0100

    Componente de Pendientes, estructura y Tabs

---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pendientes',
+  templateUrl: 'pendientes.component.html',
+})
+export class PendienteComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
---
+++ src/pages/terminados/terminados.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terminados',
+  templateUrl: 'terminados.component.html',
+})
+export class TerminadoComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
