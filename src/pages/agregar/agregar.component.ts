59edd6378dda7571e22a0d0d0454f59db885cf08
Alejandro Irimia <rimax_86@hotmail.com>
2017-12-17 20:00:57 +0100

    Agregando al servicio, alertas y retornando a la pantalla principal

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Lista, ListaItem } from '../../app/clases/index';
+import { AlertController, NavController } from 'ionic-angular';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 @Component({
   selector: 'app-agregar',
@@ -7,12 +9,16 @@ import { Lista, ListaItem } from '../../app/clases/index';
 })
 export class AgregarComponent implements OnInit {
 
-  nombreLista:string;
+  nombreLista:string = '';
   nombreItem:string = '';
 
   items:ListaItem[] = [];
 
-  constructor() {  }
+  constructor(
+    public alertCtrl: AlertController,
+    public navController: NavController,
+    public _listaDeseos:ListaDeseosService
+  ) {  }
 
   ngOnInit() {}
 
@@ -28,5 +34,21 @@ export class AgregarComponent implements OnInit {
 
   borrarItem(index:number) {
     this.items.splice(index, 1);
+  }
+
+  guardarLista() {
+    if(this.nombreLista.length === 0) {
+      let alert = this.alertCtrl.create({
+        title: 'Nombre de la lista',
+        subTitle: 'El nombre de la lista es necesario',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+    let lista = new Lista(this.nombreLista);
+    lista.items = this.items;
+    this._listaDeseos.listas.push(lista);
+    this.navController.pop();
   }
 }
