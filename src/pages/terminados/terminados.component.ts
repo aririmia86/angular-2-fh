cb839d66e9194fd8f78fd1020c21169f9b57168b
Alejandro Irimia <rimax_86@hotmail.com>
2017-12-18 20:28:52 +0100

    Solución - Pantalla de listas terminadas

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeseosService }  from "../../app/services/lista-deseos.service";
+import { NavController } from 'ionic-angular';
+import { Lista } from '../../app/clases/listas';
+import { DetalleComponent } from '../detalle/detalle.component';
 
 @Component({
   selector: 'app-terminados',
   templateUrl: 'terminados.component.html',
 })
 export class TerminadoComponent implements OnInit {
-  constructor() {  }
+  constructor(
+    private _listaDeseos:ListaDeseosService,
+    private navCtrl:NavController
+  ) {  }
 
   ngOnInit() {}
+
+  verDetalle(lista:Lista, indice:number) {
+    this.navCtrl.push(DetalleComponent, {
+      lista,
+      indice
+    });
+  }
 }
