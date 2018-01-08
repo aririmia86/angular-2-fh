2414a8f2a5abd7fc72d8de261d1475f1e7f2542d
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-08 20:06:44 +0100

    Separar las rutas hijas a un archivo especifico

--- 6. Miscelaneos/src/app/app.routes.ts
+++ 6. Miscelaneos/src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent} from './components/usuario/usuario-detalle.component';
+import { usuario_routes } from './components/usuario/usuario.routes';
 
 const app_routes: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'usuario/:id',
     component: UsuarioComponent,
-    children: [
-      { path: 'nuevo', component: UsuarioNuevoComponent },
-      { path: 'editar', component: UsuarioEditarComponent },
-      { path: 'detalle', component: UsuarioDetalleComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-    ]
+    children: usuario_routes
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
