740b7dcae59964ba9a283640676cd0700e0acd10
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-12 20:31:51 +0100

    Obteniendo el perfil del usuario que inició sesión

--- 7. Authapp/src/app/services/auth.service.ts
+++ 7. Authapp/src/app/services/auth.service.ts
@@ -6,13 +6,15 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  userProfile:any;
+
   auth0 = new auth0.WebAuth({
     clientID: '7YKXO3QSEKC2tgOXXU1g32aywPThvRs1',
     domain: 'aririmia86.eu.auth0.com',
     responseType: 'token id_token',
     audience: 'https://aririmia86.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(public router: Router) {}
@@ -57,5 +59,20 @@ export class AuthService {
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
   }
+
+  public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+}
 
 }
