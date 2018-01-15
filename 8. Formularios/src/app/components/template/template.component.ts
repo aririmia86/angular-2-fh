d36ee1e721b883ef90a7d40b0f745e7dbae74f68
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-15 19:22:27 +0100

    Template: Validaciones independientes y cambio de estilo

--- 8. Formularios/src/app/components/template/template.component.ts
+++ 8. Formularios/src/app/components/template/template.component.ts
@@ -4,14 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent {
 
   usuario:Object = {
-    nombre: 'Alejandro',
-    apellido: 'Irimia',
-    email: ''
+    nombre: null,
+    apellido: null,
+    email: null
   };
 
   constructor() { }
