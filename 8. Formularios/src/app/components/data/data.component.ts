cae5d63cd7f90dde3a7439425a8360d3a50d676d
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-17 20:34:52 +0100

    Data: Arreglos de FormControl - FormArray

--- 8. Formularios/src/app/components/data/data.component.ts
+++ 8. Formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -15,7 +15,8 @@ export class DataComponent {
       nombre: 'Alejandro',
       apellido: 'Irimia'
     },
-    correo: 'a@a.com'
+    correo: 'a@a.com',
+    // pasatiempos: ['A', 'B', 'C']
   }
 
   constructor() {
@@ -30,9 +31,18 @@ export class DataComponent {
       'correo': new FormControl('', [
         Validators.required,
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
       ])
     });
-    this.forma.setValue(this.usuario);
+    // this.forma.setValue(this.usuario);
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('Dormir', Validators.required)
+    );
   }
 
   guardarCambios() {
