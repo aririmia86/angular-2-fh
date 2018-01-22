c4af4a08682873ebeaa83fd76d8887a47d525163
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-22 19:22:50 +0100

    Creando los componentes y rutas de la aplicación

--- 9. Heroesapp/src/app/app.module.ts
+++ 9. Heroesapp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { app_routing } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
