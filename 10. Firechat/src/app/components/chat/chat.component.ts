04fbbd6f103a7554d52e7947974bcf2b91fb8664
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-28 17:09:19 +0100

    Cargar y agregar mensajes al chat

--- 10. Firechat/src/app/components/chat/chat.component.ts
+++ 10. Firechat/src/app/components/chat/chat.component.ts
@@ -11,13 +11,21 @@ export class ChatComponent {
   mensaje:string = '';
 
   constructor(public _cs:ChatService) {
-    this._cs.cargarMensajes().subscribe((mensajes:any[]) => {
-
-    });
+    this._cs.cargarMensajes().subscribe();
   }
 
   enviarMensaje() {
+    if(this.mensaje.length === 0) {
+      return;
+    }
 
+    this._cs.agregarMensaje(this.mensaje)
+      .then(() => {
+        this.mensaje = '';
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
 }
--- 10. Firechat/src/app/providers/chat.service.ts
+++ 10. Firechat/src/app/providers/chat.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Mensaje } from '../interfaces/mensaje';
 
 @Injectable()
 export class ChatService {
-  private itemsCollection: AngularFirestoreCollection<any>;
-  public chats:any[] = [];
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  public chats:Mensaje[] = [];
 
   constructor(private afs: AngularFirestore) { }
 
   cargarMensajes() {
-    this.itemsCollection = this.afs.collection<any>('chats');
-    return this.itemsCollection.valueChanges();
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    return this.itemsCollection.valueChanges()
+      .map((mensajes:Mensaje[]) => {
+        this.chats = mensajes;
+      });
+  }
+
+  agregarMensaje(texto:string) {
+    let nuevo:Mensaje = {
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+    };
+    return this.itemsCollection.add(nuevo);
   }
 
 }
