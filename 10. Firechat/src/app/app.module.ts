b85d85fe1ea16a7d051389978389822790c08264
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-28 17:42:53 +0100

    Autenticación con Google

--- 10. Firechat/src/app/app.module.ts
+++ 10. Firechat/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { ChatComponent } from './components/chat/chat.component';
 import { ChatService } from './providers/chat.service';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
