d4da34e80b9343b9178372bea8ea64ec33429761
Alejandro Irimia <rimax_86@hotmail.com>
2018-01-28 19:33:52 +0100

    ¿Cómo usar los servicios de Themoviedb?

--- 11. PeliculasApp/src/app/services/peliculas.service.ts
+++ 11. PeliculasApp/src/app/services/peliculas.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Jsonp, Http} from '@angular/http';
-import 'rxjs/Rx';
+
+import { Jsonp } from "@angular/http";
+import 'rxjs/Rx'; // Map
 
 @Injectable()
 export class PeliculasService {
 
-  private apiKey:string = '172c739705f8736ebc0a48dcf00ac8c1';
-  private urlMovieDb:string = 'https://api.themoviedb.org/3';
+  private apikey:string = "172c739705f8736ebc0a48dcf00ac8c1";
+  private urlMoviedb:string = "https://api.themoviedb.org/3";
 
-  constructor(private jsonp:Jsonp, private http:Http) { }
+  constructor( private jsonp:Jsonp ) { }
 
-  getPopulares() {
-    let url = `${this.urlMovieDb}/discover/movie?sort_by=popularity.desc&api_key=${this.apiKey}&language=es&callback=JSONP_CALLBACK`;
-    return this.jsonp.get(url)
-      .map(res => res.json());
+  getPopulares(){
+
+    let url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
+  }
+
+  buscarPelicula( texto:string ){
+
+    let url = `${ this.urlMoviedb }/search/movie?query=${ texto }&sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
   }
 
 }
