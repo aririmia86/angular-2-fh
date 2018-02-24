7d189a3bc5fdbe0eb8df2e64678b6ba158d0c24f
Alejandro Irimia <rimax_86@hotmail.com>
2018-02-24 19:56:29 +0100

    Validaciones del archivo desde la directiva

--- 14. FotosFirebase/src/app/directives/ng-drop-files.directive.ts
+++ 14. FotosFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -6,12 +6,14 @@ import {
   Input,
   Output
 } from '@angular/core';
+import {FileItem} from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() archivos: FileItem[] = [];
 
   constructor() { }
 
@@ -23,6 +25,35 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseSobre.emit(false);
+  }
+
+  private _archivoPuedeSerCargado(archivo: File): boolean {
+    if (!this._archivoYaExiste(archivo.name) && this._esImagen(archivo.type)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _prevenirDetener(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoYaExiste(nombreArchivo: string): boolean {
+    for (const archivo of this.archivos) {
+      if (archivo.nombreArchivo === nombreArchivo) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private _esImagen(tipoArchivo: string): boolean {
+    return (tipoArchivo === '' || tipoArchivo === undefined)
+      ? false
+      : tipoArchivo.startsWith('image')
+      ;
   }
 
 }
