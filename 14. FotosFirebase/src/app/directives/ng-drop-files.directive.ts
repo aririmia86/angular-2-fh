206ae9a9b4c956fdcb36d6e262cbfdafd1d45c4b
Alejandro Irimia <rimax_86@hotmail.com>
2018-02-24 20:15:01 +0100

    Obtener archivos y validarlos

--- 14. FotosFirebase/src/app/directives/ng-drop-files.directive.ts
+++ 14. FotosFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -20,6 +20,7 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter(event: any) {
     this.mouseSobre.emit(true);
+    this._prevenirDetener(event);
   }
 
   @HostListener('dragleave', ['$event'])
@@ -27,12 +28,34 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit(false);
   }
 
-  private _archivoPuedeSerCargado(archivo: File): boolean {
-    if (!this._archivoYaExiste(archivo.name) && this._esImagen(archivo.type)) {
-      return true;
-    } else {
-      return false;
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any) {
+    const transferencia = this._getTransferencia(event);
+    if (!transferencia) {
+      return;
     }
+    this._extraerArchivos(transferencia.files);
+    this._prevenirDetener(event);
+    this.mouseSobre.emit(false);
+  }
+
+  private _getTransferencia(event: any) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos(archivosLista: FileList) {
+    // noinspection TsLint
+    for (const propiedad in Object.getOwnPropertyNames(archivosLista)) {
+      const archivoTemporal = archivosLista[propiedad];
+      if (this._archivoPuedeSerCargado(archivoTemporal)) {
+        const nuevoArchivo = new FileItem(archivoTemporal);
+        this.archivos.push(nuevoArchivo);
+      }
+    }
+  }
+
+  private _archivoPuedeSerCargado(archivo: File): boolean {
+    return (!this._archivoYaExiste(archivo.name) && this._esImagen(archivo.type));
   }
 
   private _prevenirDetener(event) {
