a4841f3193f60af2012ec25c982ffb331bc4a304
Alejandro Irimia <rimax_86@hotmail.com>
2018-02-25 13:04:51 +0100

    Cargar imágenes a Firebase - Storage

--- 14. FotosFirebase/src/app/services/carga-imagenes.service.ts
+++ 14. FotosFirebase/src/app/services/carga-imagenes.service.ts
@@ -15,7 +15,32 @@ export class CargaImagenesService {
   }
 
   cargarImagenesFirebase(imagenes: FileItem[]) {
-
+    const storageRef = firebase.storage().ref();
+    for (const imagen of imagenes) {
+      imagen.estaSubiendo = true;
+      if (imagen.progreso >= 100) {
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask = storageRef
+        .child(`${this.CARPETA_IMAGENES}/${imagen.nombreArchivo}`)
+        .put(imagen.archivo);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => {
+          imagen.progreso = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.error('Error al subir', error);
+        },
+        () => {
+          console.log('Imagen cargada correctamente', imagen);
+          imagen.url = uploadTask.snapshot.downloadURL;
+          imagen.estaSubiendo = false;
+          this.guardarImagen({
+              nombre: imagen.nombreArchivo,
+              url: imagen.url
+            });
+        });
+    }
   }
 
 }
