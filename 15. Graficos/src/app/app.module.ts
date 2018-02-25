2e05bd44d1bc042f5ca5de8ce51b502779dba518
Alejandro Irimia <rimax_86@hotmail.com>
2018-02-25 17:23:09 +0100

    Instalaciones necesarias - ng2-charts

--- 15. Graficos/src/app/app.module.ts
+++ 15. Graficos/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {ChartsModule} from 'ng2-charts';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
