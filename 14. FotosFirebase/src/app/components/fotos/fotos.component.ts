7c9ced507e8e24f3503337b15bab347d172f44c3
Alejandro Irimia <rimax_86@hotmail.com>
2018-02-25 13:13:21 +0100

    Mostrar las imágenes subidas a Firebase

--- 14. FotosFirebase/src/app/components/fotos/fotos.component.ts
+++ 14. FotosFirebase/src/app/components/fotos/fotos.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+export interface Item { nombre: string; url: string; }
 
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
   styles: []
 })
-export class FotosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class FotosComponent {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
   }
-
 }
